03d9bf4e5a5743536d131d6c39e73e23902373ee
Daisuke Maki <lestrrat+github@gmail.com>
2015-01-16 14:29:42 +0900

    Add a version flag

--- cmd/sharaq/sharaq.go
+++ cmd/sharaq/sharaq.go
@@ -8,19 +8,26 @@ import (
 	"github.com/Peatix/sharaq"
 )
 
+const version = "0.0.1"
+
 func main() {
 	os.Exit(_main())
 }
 
 func _main() int {
-	var cfgfile string
-	flag.StringVar(&cfgfile, "config", "etc/sharaq.json", "config file")
+	showVersion := flag.Bool("version", true, "show sharaq version")
+	cfgfile := flag.String("config", "etc/sharaq.json", "config file")
 	flag.Parse()
 
+	if *showVersion {
+		os.Stdout.WriteString("sharaq version " + version + "\n")
+		return 0
+	}
+
 	config := &sharaq.Config{}
-	log.Printf("Using config file %s", cfgfile)
-	if err := config.ParseFile(cfgfile); err != nil {
-		log.Printf("Failed to parse '%s': %s", cfgfile, err)
+	log.Printf("Using config file %s", *cfgfile)
+	if err := config.ParseFile(*cfgfile); err != nil {
+		log.Printf("Failed to parse '%s': %s", *cfgfile, err)
 		return 1
 	}
 
