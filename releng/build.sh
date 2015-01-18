f2bb986780acc04dde0ea7a537a7e8a36e54e49a
Daisuke Maki <lestrrat+github@gmail.com>
2015-01-18 16:07:53 +0900

    Save artifacts to host so we can use for debugging

--- releng/build.sh
+++ releng/build.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-source $(cd $(dirname $BASH_SOURCE); pwd -P)/config.sh
+thisdir=$(cd $(dirname $BASH_SOURCE); pwd -P)
+source $thisdir/config.sh
 
 MOUNT_DIR=$(cd $(dirname $0)/..; pwd -P)
 id=$(echo $(date) $$| shasum | awk '{print $1}')
 docker run --rm \
     --name sharaq-build-$id \
     -v $MOUNT_DIR:$WORKDIR \
+    -v $thisdir/artifacts:$RESULTSDIR \
     -e RESULTSDIR=$RESULTSDIR \
     sharaq-docker \
     ./build-on-docker.sh
