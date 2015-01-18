d75dd170bc543f3f420de13eb7f7a24a18213107
Daisuke Maki <lestrrat+github@gmail.com>
2015-01-18 20:16:59 +0900

    Change docker build tools to reflect new packages used

--- releng/build-on-docker.sh
+++ releng/build-on-docker.sh
@@ -6,7 +6,7 @@ source $(cd $(dirname $BASH_SOURCE); pwd -P)/config.sh
 
 pushd $WORKDIR
 
-for proj in github.com/goamz/goamz/aws github.com/goamz/goamz/s3 github.com/disintegration/imaging github.com/bradfitz/gomemcache/memcache github.com/lestrrat/go-server-starter; do
+for proj in github.com/goamz/goamz/aws github.com/goamz/goamz/s3 github.com/disintegration/imaging github.com/bradfitz/gomemcache/memcache github.com/lestrrat/go-server-starter github.com/lestrrat/go-apache-logformat github.com/lestrrat/go-file-rotatelogs; do
     echo " + go get -u $proj"
     go get -u $proj
 done
