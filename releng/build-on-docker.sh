ed6fdd201d54a975961fc381dcc44f8ce354d1d9
Daisuke Maki <lestrrat+github@gmail.com>
2015-01-19 11:36:22 +0900

    Fix the golang deps at image creation time

--- releng/build-on-docker.sh
+++ releng/build-on-docker.sh
@@ -6,10 +6,10 @@ source $(cd $(dirname $BASH_SOURCE); pwd -P)/config.sh
 
 pushd $WORKDIR
 
-for proj in github.com/goamz/goamz/aws github.com/goamz/goamz/s3 github.com/disintegration/imaging github.com/bradfitz/gomemcache/memcache github.com/lestrrat/go-server-starter github.com/lestrrat/go-apache-logformat github.com/lestrrat/go-file-rotatelogs; do
-    echo " + go get -u $proj"
-    go get -u $proj
-done
+# for proj in github.com/goamz/goamz/aws github.com/goamz/goamz/s3 github.com/disintegration/imaging github.com/bradfitz/gomemcache/memcache github.com/lestrrat/go-server-starter github.com/lestrrat/go-apache-logformat github.com/lestrrat/go-file-rotatelogs; do
+#    echo " + go get -u $proj"
+#    go get -u $proj
+#done
 
 goxc \
     -tasks "xc archive" \
