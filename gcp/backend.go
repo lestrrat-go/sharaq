48c07b205796ab4bc7d4c72032ae3b18807e2d5c
Daisuke Maki <lestrrat+github@gmail.com>
2016-12-22 14:53:24 +0900

    standardize on path generation

--- gcp/backend.go
+++ gcp/backend.go
@@ -73,7 +73,7 @@ func (s *StorageBackend) Serve(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// create the proper url
-	specificURL := u.Scheme + "://storage.googleapis.com/" + s.bucketName + "/" + preset + u.Path
+	specificURL := u.Scheme + "://storage.googleapis.com/" + makeStoragePath(preset, u)
 
 	log.Printf("Making HEAD request to %s...", specificURL)
 	res, err := http.Head(specificURL)
