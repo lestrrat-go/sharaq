afd72e106eb262097623af2009c6f411ada8d7b4
Daisuke Maki <lestrrat+github@gmail.com>
2017-02-15 15:41:08 +0900

    remove outdated files

--- releng/build-image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-set -e
-docker build -t sharaq-docker .
--- releng/config.sh
+++
@@ -1,6 +0,0 @@
-export WORKDIR=/work/src/github.com/lestrrat/sharaq
-export RESULTSDIR=/work/artifacts
-export PROJECTDIR=$(cd $(dirname $BASH_SOURCE)/..; pwd -P)
-if [ -z "$RELEASE_VERSION" ]; then
-    export RELEASE_VERSION=$(grep version $PROJECTDIR/cmd/sharaq/sharaq.go | perl -ne 'print $1 if /version\s+=\s+"([^"]+)"/')
-fi
